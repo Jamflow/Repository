9de6caa359cba650a6206ded57a9a4ddf23f3cbe
jamflowe <jamflowe@gmail.com>
2016-11-08 23:29:00 +0000

    update

--- todolist.js
+++ todolist.js
@@ -3,21 +3,22 @@ var taskColor = document.getElementById("taskColor");
 var newTask = document.getElementById("newTask");
 var taskButton = document.getElementById("taskButton");
 
+/*Adds event listener for clicks upon task button and creates new tasks.*/
 taskButton.addEventListener("click", function() {
     
     var color = taskColor.value
     var task = "<div style='color:"+color+"'>"+actionInput.value+"</div>";
     
-    
     newTask.innerHTML = newTask.innerHTML+task;
     
 });
 
+/*Creates function that removes task when clicked.*/
 newTask.addEventListener("click",function(evt){
 
-    var thing = evt.target;
+    var existingTask = evt.target;
 
-    thing.parentNode.removeChild(thing);
+    existingTask.parentNode.removeChild(existingTask);
         
 });
     
